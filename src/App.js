d6746857a565da9660a892017bbbfd8fcc052089
John Long <johnlong@amcsgroup.com>
2021-03-17 18:51:41 +0000

    rendering a list

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   state = {
     persons: [
       {name: 'John', age:28 },
-      {name: 'J', age:28 },
-      {name: 'Jo', age:28 }
+      {name: 'J', age:29 },
+      {name: 'Jo', age:30 }
     ],
     otherState: 'Some other value',
     showPersons: false
@@ -49,28 +49,27 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div >
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              age={person.age}/>
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <button 
           style={style}
-          onClick={this.togglePersonsHandler}>Switch name</button>
-        { 
-          this.state.showPersons ? 
-            <div >
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, 'Jimmy')} >My hobbies: Programming</Person> 
-            <Person 
-              name="J" 
-              age="65"
-              changed={this.nameChangedHandler}></Person>
-            <Person 
-              name="Jo" 
-              age="72"/>
-          </div> : null
-        }
+          onClick={this.togglePersonsHandler}>Toggle persons</button>
+       {persons}
       </div>
     );
   }
